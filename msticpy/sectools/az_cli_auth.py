9bc46db31e75e47f06657ef59d6aca7bff5226a6
Ian Hellen <ianhelle@microsoft.com>
2021-07-29 18:24:26 -0700

    Start of azure cli logon for AML

---
+++ msticpy/sectools/az_cli_auth.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""Azure CLI authentication module."""
+# import subprocess
+from .._version import VERSION
+
+__version__ = VERSION
+__author__ = "Ian Hellen"
